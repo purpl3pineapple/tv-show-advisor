c72e596ed894cbfbe7ca47286585f4bfbe12c8d3
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-09 04:00:21 -0400

    removed api key from public view

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -5,21 +5,21 @@ import { Config } from "../config";
 export class TVShowAPI {
 
     static async fetchPopulars(){
-        const response = await axios.get(`${Config.BASE_URL}/tv/popular${Config.API_KEY}`);
+        const response = await axios.get(`${Config.BASE_URL}/tv/popular?api_key=${process.env.REACT_APP_API_KEY}`);
         console.info(response.data.results); 
         return response.data.results;
         //return FAKE_POPULARS;
     };
 
     static async fetchRecommendations(tvShowID){
-        const response = await axios.get(`${Config.BASE_URL}/tv/${tvShowID}/recommendations${Config.API_KEY}`);
+        const response = await axios.get(`${Config.BASE_URL}/tv/${tvShowID}/recommendations?api_key=${process.env.REACT_APP_API_KEY}`);
         console.info(response.data.results);
         return response.data.results;
         //return FAKE_RECOMMENDATIONS;
     };
 
     static async fetchByTitle(title){
-        const response = await axios.get(`${Config.BASE_URL}search/tv${Config.API_KEY}&query=${title}`);
+        const response = await axios.get(`${Config.BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY}&query=${title}`);
         console.info(response.data.results);
         return response.data.results;
         //return FAKE_RECOMMENDATIONS;
